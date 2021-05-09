daf5b7ef0154b666ddf179c383d5e6823878898e
Christian Pereira <christian@Christians-MacBook-Air.local>
2021-05-08 22:05:00 -0300

    wip: cria consultas por id, limit, consulta comum e com filtros

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const MongoClient = require("mongodb").MongoClient;
+const assert = require('assert');
 const circulationRepo = require('./repositories/circulationRepo');
 const data = require('./circulation.json');
 
@@ -7,13 +8,32 @@ const dbName = 'circulation';
 async function main() {
     const client = new MongoClient(url);
     await client.connect();
+    try {
+        const results = await circulationRepo.loadData(data);
+        assert.equal(data.length, results.insertedCount);
 
-    const results = await circulationRepo.loadData(data);
-    console.log(results.insertedCount, results.ops);
-    const admin = client.db(dbName).admin();
+        const getData = await circulationRepo.get();
+        assert.equal(data.length, getData.length);
 
-    console.log(await admin.listDatabases())
+         const filterData = await circulationRepo.get(getData[4]);
+         assert.deepEqual(filterData[0], getData[4]);
 
+        const limitData = await circulationRepo.get({}, 3);
+        assert.equal(limitData.length, 3);
+
+        const id = getData[4]._id.toString()
+        const byId = await circulationRepo.getById(id);
+        assert.deepEqual(byId, getData[4])
+
+        // console.log("getData", getData.length)
+    } catch (error) {
+        console.log(error)
+    } finally {
+        const admin = client.db(dbName).admin();
+        await client.db(dbName).dropDatabase();
+        console.log(await admin.listDatabases())
+        client.close();
+    }
 }
 
 main();
--- repositories/circulationRepo.js
+++ repositories/circulationRepo.js
@@ -1,8 +1,45 @@
-const { MongoClient } = require("mongodb");
+const { MongoClient, ObjectID } = require("mongodb");
 
 function circulationRepo() {
     const url = "mongodb://localhost:27017";
     const dbName = 'circulation';
+
+    function get(query, limit) {
+        return new Promise(async (resolve, reject) => {
+            const client = new MongoClient(url);
+            try {
+                await client.connect();
+                const db = client.db(dbName);
+
+                let item = db.collection("newspappers").find(query);
+
+                if (limit > 0) {
+                    item.limit(limit);
+                }
+
+                resolve(await item.toArray());
+                client.close();
+            } catch (error) {
+                reject(error)
+            }
+        })
+    }
+
+    function getById(id) {
+        return new Promise(async (resolve, reject) => {
+            const client = new MongoClient(url);
+            try {
+                await client.connect();
+                const db = client.db(dbName);
+                let item = await db.collection("newspappers").findOne({_id: ObjectID(id)});
+
+                resolve(await item);
+                client.close();
+            } catch (error) {
+                reject(error)
+            }
+        })
+    }
 
     function loadData(data) {
         return new Promise(async (resolve, reject) => {
@@ -12,14 +49,15 @@ function circulationRepo() {
                 const db = client.db(dbName);
 
                 results = await db.collection('newspappers').insertMany(data);
-                resolve(results)
+                resolve(results);
+                client.close();
             } catch (error) {
                 reject(error)
             }
         })
     }
 
-    return { loadData }
+    return { loadData, get, getById }
 }
 
 module.exports = circulationRepo();
